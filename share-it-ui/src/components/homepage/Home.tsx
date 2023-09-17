8368d97295ea1ae7e55a8de15da646b9abbace8c
kiowesmith21 <kiowesmith@gmail.com>
2023-09-17 17:54:40 -0400

    add components to homepage

--- share-it-ui/src/components/homepage/Home.tsx
+++ share-it-ui/src/components/homepage/Home.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import './Home.css'
 import logo from './logo.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../navbar/Navbar';
 
 const Home = () => {
   return (
     <div className="home">
       <header className="home-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" >
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">ShareIt</a>
+            <a className="nav-link active" aria-current="page" href="#">About</a>
+          </div>
+        </nav>
       </header>
-      <div className='login-box'>
-      <div className="mb-3">
-        <label className="form-label">Your vanity URL</label>
-        <div className="input-group">
-            <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
-        </div>
+      <div className='content'>
+        <img src={logo} className="home-logo" alt="logo" />
+        <div className='login-box'>
+          <div className="mb-3">
+            <label className="form-label">Username:</label>
+            <div className="input-group">
+                <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+            </div>
+            <label className="form-label">Password:</label>
+            <div className="input-group">
+                <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" />
+            </div>
+          </div>
+            <button type="button" className="btn btn-primary">Login</button>
+            <button type="button" className="btn btn-primary">Register</button>
         </div>
       </div>
     </div>
