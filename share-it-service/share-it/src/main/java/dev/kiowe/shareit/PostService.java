ee0eb63815927611184a324122355d30d1cb1984
kiowesmith21 <kiowesmith@gmail.com>
2023-09-23 15:46:45 -0400

    finish service

---
+++ share-it-service/share-it/src/main/java/dev/kiowe/shareit/PostService.java
@@ -0,0 +1,29 @@
+package dev.kiowe.shareit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Post createPost(String userName, String postBody, String img) {
+        Post post = postRepository.insert(new Post(postBody, img));
+
+        mongoTemplate.update(User.class)
+                .matching(Criteria.where("userName").is(userName))
+                .apply(new Update().push("postIds").value(post))
+                .first();
+
+        return post;
+    }
+
+}
